ed1487976ff908078d665d6b5467c2adf1db41fa
Trista Runions <t.runions19@gmail.com>
2021-04-15 21:10:44 -0700

    finished task 4

--- index.js
+++ index.js
@@ -120,7 +120,15 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
+   constructor(attrs){
+     this.name = attrs.name;
+     this.age = attrs.age;
+     this.location = attrs.location;
+   }
     
+   speak(){
+     return `Hello my name is ${this.name}, I am from ${this.location}`
+   }
   }
   
   /*
@@ -137,9 +145,25 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian{
+   constructor(attrs){
+     super(attrs);
+     this.specialty = attrs.specialty;
+     this.favLanguage = attrs.favLanguage;
+     this.catchPhrase = attrs.catchPhrase;
 
- }
+     }
+
+     demo(subject){
+       return `Today we are learning about ${subject}`
+     }
+
+     grade(student, subject){
+       return `${student} receives a perfect score on ${subject}`
+     }
+   }
+
+ 
   /*
     TASK 5
       - Write a Student class extending Lambdasian.
